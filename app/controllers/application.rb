35e8338111f0cd71a96f93497fbd612fa4dab0f1
Carl Tanner <captproton@betterbilling.net>
2008-05-21 10:20:05 -0700

    add directories and contents

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '34aa8c8a9dde865b5ec1d575008a37b4'
+end
