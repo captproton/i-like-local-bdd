0839b8009bc99c7c41a99ba621f53b930812cb8f
Carl Tanner <captproton@betterbilling.net>
2008-05-22 09:40:01 -0700

    made changes in app to start mirroring caboo.se sample app

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,9 +2,44 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
+  include AuthenticatedSystem
+  include ExceptionNotifiable
 
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '34aa8c8a9dde865b5ec1d575008a37b4'
+  class AccessDenied < StandardError; end
+
+  # Pick a unique cookie name to distinguish our session data from others'
+  session :session_key => '_trunk_session_id'
+
+  # If you want timezones per-user, uncomment this:
+  #before_filter :login_required
+
+  around_filter :set_timezone
+  around_filter :catch_errors
+  
+  protected
+    def self.protected_actions
+      [ :edit, :update, :destroy ]
+    end
+
+  private
+
+    def set_timezone
+      TzTime.zone = logged_in? ? current_user.tz : TimeZone.new('Etc/UTC')
+        yield
+      TzTime.reset!
+    end
+
+    def catch_errors
+      begin
+        yield
+
+      rescue AccessDenied
+        flash[:notice] = "You do not have access to that area."
+        redirect_to '/'
+      rescue ActiveRecord::RecordNotFound
+        flash[:notice] = "Sorry, can't find that record."
+        redirect_to '/'
+      end
+    end
+
 end
